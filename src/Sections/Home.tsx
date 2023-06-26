a74edb80e302ae050673aa48a4ba1e1c31d62d78
snigbar <snigbar@outlook.com>
2023-06-26 23:01:58 +0600

    header section completed

--- src/Sections/Home.tsx
+++ src/Sections/Home.tsx
@@ -8,6 +8,7 @@ import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPageType } from "@/shared/types";
 import ActionButton from "@/components/ActionButton";
+import { motion } from "framer-motion";
 
 
 type Props = {
@@ -21,11 +22,21 @@ const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
     <section id='home' className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
     {/* header */}
-    <div className="md:flex mx-auto w-5/6 items-center justify-center">
+    <motion.div className="md:flex mx-auto w-5/6 items-center justify-center" onViewportEnter={() => setSelectedPage(SelectedPageType.Home)}>
         {/* header text */}
         <div className="z-10 mt-32 md:basis-3/5">
             {/* text */}
-            <div className="md:-mt-20">
+            <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            >
                 <div className="relative">
                     <div className="before:absolute before:-mt-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                         <img alt="home-page-text" src={HomePageText}></img>
@@ -37,24 +48,33 @@ const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
                 Studios to get the Body Shapes That you Dream of.. Get Your Dream
                 Body Now.
                 </p>
-            </div>
+            </motion.div>
             {/* action button */}
-            <div className="mt-8 flex items-center gap-8">
+            <motion.div className="mt-8 flex items-center gap-8" 
+             initial="hidden"
+             whileInView="visible"
+             viewport={{ once: true, amount: 0.5 }}
+             transition={{ delay: 0.5, duration: 0.5 }}
+             variants={{
+               hidden: { opacity: 0, x: 50 },
+               visible: { opacity: 1, x: 0 },
+             }}
+            >
                 <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                 <AnchorLink className="text-sm cursor-pointer text-primary-500 underline" href={`#${SelectedPageType.ContuctUs}`} onClick={() => setSelectedPage(SelectedPageType.ContuctUs)}>Contact Us</AnchorLink>
-            </div>
+            </motion.div>
         </div>
 
         {/* header image */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
             <img src={HomePageGraphic} alt="home-page-graphic" />
         </div>
-    </div>
+    </motion.div>
 
     {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
-            <div className="flex w-3/5 items-center justify-between gap-8">
+            <div className="flex w-3/5 items-center justify-between gap-8 mx-auto">
               <img alt="redbull-sponsor" src={SponsorRedBull} />
               <img alt="forbes-sponsor" src={SponsorForbes} />
               <img alt="fortune-sponsor" src={SponsorFortune} />
